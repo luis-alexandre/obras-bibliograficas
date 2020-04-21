e7ce95c252b932f4117e368cd10672844934dcd2
Luis Alexandre Rodrigues <U001431x@xpcorretora.com.br>
2020-04-21 13:17:23 -0300

    Criando container para a web api

---
+++ src/deploy.ps1
@@ -0,0 +1,3 @@
+﻿dotnet restore .\webapi\Guide.ObrasLiterarias.Api
+dotnet publish .\webapi\Guide.ObrasLiterarias.Api -c release -o ./publish -r linux-x64 
+docker build -t .\webapi\Guide.ObrasLiterarias.Api guide/webapi:1.0 . 
